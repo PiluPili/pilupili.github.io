edea716eb5e8da2b39fff41607b692c816755a88
Pilar <pilihuppi@gmail.com>
2022-06-05 18:36:16 -0300

    modified:   javascript.js

--- javascript.js
+++ javascript.js
@@ -1,33 +1,33 @@
-//let people = []
-//let amount = []
 var personas = 0
 var montototal = 0
 
 function getinputs(){
-    //people.push(document.getElementById("InputNombre"));
-    //amount.push(document.getElementById("InputConsumo").value);
-    var nombre = document.getElementById("InputNombre");
+    var nombre = document.getElementById("InputNombre").value;
     var consumo = document.getElementById("InputConsumo").value;
     personas = personas + 1;
-    montototal = montototal + parseFloat(consumo)
+    montototal = montototal + parseFloat(consumo);
+    
 }
 
-function listado(consumo, nombre){
-    var listado = document.getElementById("Listado")
+function listado(consumo){
+    var listado = document.getElementById("Listado");
     if (consumo !=0 && nombre !=""){
-        var perscons = nombre.parseFloat + " $ " + consumo.parseFloat
-        listado.appendChild(nombre + "$")
-    }
-    //document.getElementById("listado").innerText   
+        var lista = createElement("lista");
+        lista.innerText = nombre + " $ " + consumo.parseFloat();
+        listado.appendChild(lista)
+    } 
 }
+
 
 function dividir(montototal, personas){
     var total = montototal/personas
     document.getElementById("aporte").innerText = total
+    prompt(total)
 }
 
 
 function resolver() {
     getinputs();
+    listado()
     dividir();
 }

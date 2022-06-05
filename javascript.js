ea6ba57b68004e4e8286d95cbfa313109b61fc9c
Pilar <pilihuppi@gmail.com>
2022-06-05 17:50:39 -0300

    deleted:    README.md 	modified:   index.html 	modified:   javascript.js

--- javascript.js
+++ javascript.js
@@ -1,32 +1,33 @@
-let people = []
-let amount = []
-personas = 0
-montototal = 0
+//let people = []
+//let amount = []
+var personas = 0
+var montototal = 0
 
 function getinputs(){
-    if  (personas != 0)
-    people.push(document.getElementById("InputNombre"));
-    amount.push(document.getElementById("InputConsumo").value);
+    //people.push(document.getElementById("InputNombre"));
+    //amount.push(document.getElementById("InputConsumo").value);
+    var nombre = document.getElementById("InputNombre");
+    var consumo = document.getElementById("InputConsumo").value;
     personas = personas + 1;
-    montototal = montototal + parseFloat(consumo);
-    console.log(personas)
+    montototal = montototal + parseFloat(consumo)
+}
+
+function listado(consumo, nombre){
+    var listado = document.getElementById("Listado")
+    if (consumo !=0 && nombre !=""){
+        var perscons = nombre.parseFloat + " $ " + consumo.parseFloat
+        listado.appendChild(nombre + "$")
+    }
+    //document.getElementById("listado").innerText   
 }
 
 function dividir(montototal, personas){
     var total = montototal/personas
+    document.getElementById("aporte").innerText = total
 }
 
-function resultado() {
-    display = "A cada uno le toca aportar $" + total.value;
-    document.getElementById("aporte").innerText = display;
-}
-
-//function lista(){
-   // document.getElementById("listado").innerText = 
-//}
 
 function resolver() {
     getinputs();
     dividir();
-    resultado();
 }
